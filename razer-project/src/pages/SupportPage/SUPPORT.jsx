c88639c4a3311bfaed65ef766f151db1ee42cad9
praveen3411 <ypraveen341@gmail.com>
2023-01-04 15:47:01 +0530

    All Pages Offers Added

--- razer-project/src/pages/SupportPage/SUPPORT.jsx
+++ razer-project/src/pages/SupportPage/SUPPORT.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
+import ShowingOffer from "../../components/EverywhereUse/ShowingOffer";
 import SupportNavbarData from "../../components/StoreNavbar/Support.Navbar";
 
 export default function SupportPage() {
   return (
     <Box>
       <SupportNavbarData />
+      <ShowingOffer passoffer={"Here you can see all the projects"}/>
       <h1 style={{ color: "white" }}>Support</h1>
     </Box>
   );
